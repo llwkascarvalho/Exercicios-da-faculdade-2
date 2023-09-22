6281609dfee43f3a6c349d345b920c9c8c5ce430
Lwkas Carvalho <lwkascarvalho1@gmail.com>
2023-09-22 16:39:09 -0300

    Update contaBanco.py

--- Aula de Conceitos/contaBanco.py
+++ Aula de Conceitos/contaBanco.py
@@ -1,15 +1,19 @@
 class Conta: 
     
-    def __init__(self, numero, titular, saldo, limite, nome_tipo, codigo_tipo):
+    def __init__(self, numero, titular, saldo, limite, codigo_tipo):
         self.numero = numero
         self.titular = titular
         self.saldo = saldo
         self.limite = limite
-        self.codigo_tipo = codigo_tipo
-        self.nome_tipo = nome_tipo
+        self.codigo_tipo = codigo_tipo if codigo_tipo == 1 or codigo_tipo == 2 else False
+        self.nome_tipo = "corrente" if codigo_tipo == 1 else "poupança"
 
     def depositar(self, valor):
-        self.saldo += valor
+        if (valor > self.limite):
+            return False
+        else:
+            self.saldo += valor
+            return True
     
     def sacar(self, valor):
         if (self.saldo < valor):
@@ -19,26 +23,45 @@ class Conta:
             return True
         
     def transferir (self, valor, destino):
-        if self.sacar(valor):
-            destino.depositar(valor)
-            print(f"Transferência de R${valor} realizada com sucesso para a conta de {destino.titular}.")
-        else:
-            print("Saldo insuficiente para a transferência.")
+        if(self.saldo < valor):
+            return "Saldo insuficiente"
+        elif (self.limite < valor):
+            return "Valor excedido."
+        else: 
+            self.saldo -= valor
+            destino.saldo += valor
         
     def extrato(self):
         print("Número da Conta: {}\nTitular: {}\nSaldo: {}".format(self.numero, self.titular, self.saldo))
         print("Tipo de Conta: {} - {}".format(self.codigo_tipo, self.nome_tipo))
-    
-print ("\n01- Conta Corrente\n02- Conta Poupança")
-codigo_tipo = input("Qual tipo de conta deseja criar (01 para Conta Corrente, 02 para Conta Poupança)? ")
-nome_tipo = "Conta Corrente" if codigo_tipo == "01" else "Conta Poupança"
 
-numero_conta = input("Digite o número da conta: ")
-titular_conta = input("Digite o nome do titular da conta: ")
-saldo_conta = float(input("Digite o saldo da conta: "))
-limite_conta = float(input("Digite o limite da conta: "))
 
-conta = Conta(numero_conta, titular_conta, saldo_conta, limite_conta, codigo_tipo, nome_tipo)
+conta1 = Conta(
+    int(input("Digite o número da conta: ")),
+    str(input("Digite o nome do titular da conta: ")),
+    float(input("Digite o saldo da conta: ")),
+    float(input("Digite o limite da conta: ")),
+    int(input("Tipo da conta 01 para poupança, 02 para corrente: "))
+)
 
-print("\nExtrato da conta:")
-conta.extrato()
+conta2 = Conta(
+    int(input("Digite o número da conta: ")),
+    str(input("Digite o nome do titular da conta: ")),
+    float(input("Digite o saldo da conta: ")),
+    float(input("Digite o limite da conta: ")),
+    int(input("Tipo da conta 01 para poupança, 02 para corrente: "))
+)
+
+valorTrans = float(input("valor da transferencia: "))
+
+opc = int(input("Conta que deseja realizar a transferencia: "))
+
+if (opc == 1):
+    conta1.transferir (valorTrans, conta2)
+elif (opc == 2):
+    conta2.transferir (valorTrans, conta1)
+else:
+    print ("invalido")
+
+print (conta1.saldo)
+print (conta2.saldo)
