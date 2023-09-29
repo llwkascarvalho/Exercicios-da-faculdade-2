4e0d9122f589180112ebdab9b3e4b9debc325941
Lwkas Carvalho <lwkascarvalho1@gmail.com>
2023-09-28 23:25:39 -0300

    conclusão da atividade conta.py

--- Atividade Guiada/conta.py
+++ Atividade Guiada/conta.py
@@ -1,22 +1,28 @@
+import datetime
+
 class Conta:
     def __init__(self, numero, titular, saldo, limite):
         self.numero = numero
         self.titular = titular
         self.saldo = saldo
         self.limite = limite
+        self.data_abertura = datetime.date.today()
+        self.historico = Historico()
 
     def depositar(self,valor):
         self.saldo += valor
+        self.historico.transacoes.append(f"Depósito de R${valor:.2f} em {self.data_abertura}")
 
     def saca(self, valor):
         if (self.saldo < valor):
             return False
         else:
             self.saldo -= valor
+            self.historico.transacoes.append(f"Saque de R${valor:.2f} em {self.data_abertura}")
             return True
 
     def extrato(self):
-        print ("Número: {} \nSaldo: {}\n".format(self.numero, self.saldo, ))
+        print("Número: {}\nSaldo: {}\nTitular: {} {}\nCPF: {}".format(self.numero, self.saldo, self.titular.nome, self.titular.sobrenome, self.titular.cpf))
 
     def transfere_para(self, destino, valor):
         retirou = self.saca(valor)
@@ -32,6 +38,14 @@ class Cliente:
         self.nome = nome
         self.sobrenome = sobrenome
         self.cpf = cpf
+
+class Historico:
+    def __init__(self):
+        self.transacoes = []
+
+    def imprime(self):
+        for transacao in self.transacoes:
+            print(transacao)
 
     
 
--- Atividade Guiada/conta_teste.py
+++ Atividade Guiada/conta_teste.py
@@ -4,5 +4,9 @@ from conta import Cliente
 cliente = Cliente('Lwkas', 'Carvalho', 123456)
 conta = Conta('1234-5',cliente,100,1000)
 
-print(conta.extrato)
+conta.depositar(500)
+conta.saca(200)
 
+conta.extrato()
+
+conta.historico.imprime()
