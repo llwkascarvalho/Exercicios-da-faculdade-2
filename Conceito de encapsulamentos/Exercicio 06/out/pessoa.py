239aac3d0f09a5c04b91479a60b630c3af887f7f
Lwkas Carvalho <lwkascarvalho1@gmail.com>
2023-10-17 01:54:05 -0300

    sucess pessoa.py

--- Conceito de encapsulamentos/Exercicio 06/out/pessoa.py
+++ Conceito de encapsulamentos/Exercicio 06/out/pessoa.py
@@ -19,9 +19,39 @@ class Jogador:
     def set_nascimento(self, novo_nascimento):
         self._nascimento = novo_nascimento
 
-    def calcular_idade(self, nascimento):
-        self.idade = nascimento - 2023 
-        print(f"Idade: {self.idade}")
+    def calcular_idade(self):
+        self.idade = 2023 - self._nascimento 
+
+    def calcular_idade_aposentadoria(self):
+        if (self._posicao == 'defesa'):
+            self.idade_aposentadoria = 40
+        elif(self._posicao == 'meio-campo'):
+            self.idade_aposentadoria = 38
+        elif self._posicao == 'atacante':
+            self.idade_aposentadoria = 35
+        else:
+            self.idade_aposentadoria = 0
+
+        self.idade_aposentadoria = self.idade_aposentadoria
+        self.calcular_idade()
+        self.idade_restante = self.idade - self.idade_aposentadoria
+            
+
+    def exibir(self):
+        self.calcular_idade()
+        self.calcular_idade_aposentadoria()
+        print(f"\nNome do jogador: {self.nome}\nPosição: {self.get_posicao()}\nNascimento: {self.get_nascimento()}\nNacionalidade: {self.nacionalidade}\nAltura: {self.altura}\nPeso: {self.peso}\nIdade: {self.idade}\nIdade de aposentadoria: {self.idade_aposentadoria}\nAnos restantes até a aposentadoria: {self.idade_restante}\n")
 
 jogador = Jogador(nome = 'Lwkas', posicao='Zagueiro', nascimento= 2004, nacionalidade='Brasileiro', altura=1.60,peso=58)
 
+opc = int(input(("\nO QUE DESEJA FAZER?\n1- Calcular idade.\n2- Calcular idade até aposentadoria.\n3- Exibir informações do jogador.\n")))
+if opc == 1:
+    jogador.calcular_idade()
+    print(f"Idade: {jogador.idade}")
+elif (opc == 2):
+    print("\nDefesa - 40 anos\nMeio-campo - 38 anos\nAtacante 35 anos.")
+    jogador.set_posicao(str(input("Digite a posição: ")))
+    jogador.calcular_idade_aposentadoria()
+    print(f"Faltam {jogador.idade_restante} anos até a aposentadoria.\nVocê possui {jogador.idade}")
+elif(opc == 3):
+    jogador.exibir()
