4d91dfe7a3e64cdd3ba63fbf6beaea0925e308ee
Lwkas Carvalho <lwkascarvalho1@gmail.com>
2023-10-19 00:26:18 -0300

    ultima versão
    Método Inicializar finalizado.
    Método Entrar finalizado.
    Método Sair com erro.
    Método Subir e Descer com erro.

--- Conceito de encapsulamentos/Exercicio 06/out/elevador.py
+++ Conceito de encapsulamentos/Exercicio 06/out/elevador.py
@@ -3,27 +3,63 @@
 class Elevador:
     def __init__(self):
         self.andar_atual = 0
-        self.total_andares_prédio = 10
-        self.capacidade_elevador = 10
-        self.qtd_pessoas_elevador = 0
+        self.total_andares = 0
+        self.capacidade = 0
+        self.qtd_pessoas = 0
 
-    def inicializar(self):
-        pass
+    def inicializar(self, capacidade, total_andares):
+        self.capacidade = capacidade
+        self.total_andares = total_andares
         # que deve receber como parâmetros a capacidade do elevador e o total de andares no prédio (os elevadores sempre começam no térreo e vazio);
     def entrar(self):
-        pass
+        if int(self.qtd_pessoas) > int(self.capacidade):
+            print("Não há espaço no elevador.", self.qtd_pessoas)
+        else:
+            self.qtd_pessoas += 1
+            print("Pode entrar!", self.qtd_pessoas)
+
         #  para acrescentar uma pessoa no elevador (só deve acrescentar se ainda houver espaço)
     def sair(self):
-        pass
+        if self.qtd_pessoas > 0:
+            self.qtd_pessoas - 1
+            print("Pessoas no elevador: ",self.qtd_pessoas)
         # para remover uma pessoa do elevador (só deve remover se houver alguém dentro dele);
-    def subir(self):
-        pass
+    def subir(self, andar_destino):
+        print("Estamos no andar:", self.andar_atual)
+        if int(self.andar_atual) < int(self.total_andares):
+            self.andar_atual += andar_destino
+            print("Subindo para o andar:", self.andar_atual)
+        else:
+            print("Já estamos no último andar. Não é possível subir mais.")
         # para subir um andar (não deve subir se já estiver no último andar);
-    def descer(self):
-        pass
+    def descer(self,descer):
+        print("Estamos no andar: ", self.andar_atual)
+        self.andar_atual -= descer
+        print("Agora estamos no andar: ",self.andar_atual)
         # para descer um andar (não deve descer se já estiver no térreo);
 
 # Obs.: Encapsular todos os atributos da classe (criar os métodos set e get).
 
-while True: 
-    pass
+
+elevador = Elevador()
+while True:
+    opc = input("\nO que deseja?\n1- Inicializar\n2- Entrar no elevador\n3- Sair do elevador\n4- Subir\n5- Descer\n6- Parar execução\n")
+
+    if opc == '1':
+        total_andares = input("Qual a quantidade de andares? ")
+        capacidade = input("Capacidade: ")
+        elevador.inicializar(capacidade, total_andares)
+    elif opc == '2':
+        elevador.entrar()
+    elif opc == '3':
+        elevador.sair()
+    elif opc == '4':
+        andares = int(input("Quantos andares deseja subir? "))
+        elevador.subir(andares)
+    elif opc == '5':
+        andares = int(input("Quantos andares deseja descer? "))
+        elevador.descer(andares)
+    elif opc == '6':
+        print("Fim")
+        break
+        
