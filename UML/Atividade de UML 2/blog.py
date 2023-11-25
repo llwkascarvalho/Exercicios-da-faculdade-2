93521b773386d4b6ec9e84dd070285aec9420f8c
Lwkas Carvalho <lwkascarvalho1@gmail.com>
2023-11-25 17:13:53 -0300

    Create blog.py

---
+++ UML/Atividade de UML 2/blog.py
@@ -0,0 +1,20 @@
+class Blog:
+    def __init__(self) -> None:
+        pass
+
+    def listarPostagemPublicadas(): 
+        pass
+
+    def listarTodasAsPostagens():
+        pass
+
+    def apagarPostagem():
+
+
+class Postagem:
+    def __init__(self) -> None:
+        pass
+
+class Usuario: 
+    def ___init__(self) -> None:
+        pass
