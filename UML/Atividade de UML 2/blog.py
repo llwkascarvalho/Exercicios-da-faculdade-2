44744fbfd1ba4fd44e0472e6ebc88bcbb4e60f10
Lwkas Carvalho <lwkascarvalho1@gmail.com>
2023-12-01 16:06:22 -0300

    Update blog.py

--- UML/Atividade de UML 2/blog.py
+++ UML/Atividade de UML 2/blog.py
@@ -1,20 +1,47 @@
 class Blog:
-    def __init__(self) -> None:
-        pass
+    def __init__(self, postagens):
+        self.__postagens = postagens
 
-    def listarPostagemPublicadas(): 
+    def adicionarPostagem(postagem):
         pass
-
-    def listarTodasAsPostagens():
+    def publicarPostagem(postagem):
         pass
-
-    def apagarPostagem():
+    def listarPostagemPublicadas():
+        pass
+    def listarTodasAsPostagem():
+        pass
+    def apagarPostagem(postagem):
         pass
 
 class Postagem:
-    def __init__(self) -> None:
-        pass
+    def __init__(self, id, titulo, texto, dataPublicada, usuario) -> None:
+        self.__id = id
+        self.__titulo = titulo 
+        self.__texto = texto
+        self.__data = dataPublicada
+        self.__usuario = usuario
 
-class Usuario: 
-    def ___init__(self) -> None:
-        pass
+        #fazer gets e sets
+
+class MixinAutenticavel: 
+    def autenticar(usuario, loginDigitado, senhaDigitado):
+        if usuario.getLogin() == loginDigitado and usuario.getLogin() == senhaDigitado:
+            return True
+        else:
+            return False
+
+class Usuario(MixinAutenticavel): 
+    def ___init__(self, id, nome, login, senha) -> None:
+        self.__id = id
+        self.__nome = nome
+        self.__login = login 
+        self.__senha = senha
+
+class SistemaBlog: 
+    def ___init__(self, blog):
+        self.__blog = blog
+
+    def autenticar(usuario, senhaDigitado, loginDigitado):
+        return usuario.autenticar(usuario, senhaDigitado, loginDigitado)
+
+    
