19ca733dc9652b20f73c6a1bd15c441d5448a548
Lwkas Carvalho <lwkascarvalho1@gmail.com>
2024-01-11 23:53:44 -0300

    exercicios antes da prova

--- POO/aula3.py
+++ POO/aula3.py
@@ -20,3 +20,38 @@ s1 = Secretaria('2', 'Maria', '019283472', '170')
 string = s1.__str__()
 
 print(string)
+
+
+
+
+# POLIMORFISMO 
+
+class Animal:
+    def falar(self):
+        pass
+
+class Cachorro(Animal):
+    def falar(self):
+        return "Woof!"
+
+class Gato(Animal):
+    def falar(self):
+        return "Meow!"
+
+class Pato(Animal):
+    def falar(self):
+        return "Quack!"
+
+# Função que interage com diferentes tipos de animais de forma polimórfica
+def fazer_barulho(animal):
+    return animal.falar()
+
+# Criando instâncias das diferentes classes de animais
+cachorro = Cachorro()
+gato = Gato()
+pato = Pato()
+
+# Chamando a função fazer_barulho com diferentes tipos de animais
+print(fazer_barulho(cachorro))  # Saída: "Woof!"
+print(fazer_barulho(gato))      # Saída: "Meow!"
+print(fazer_barulho(pato))      # Saída: "Quack!"
--- POO/composicao_carro.py
+++ POO/composicao_carro.py
@@ -1,5 +1,7 @@
+# COMPOSIÇÃO
+
 class Acelerador:
-     def acelerar(self):
+    def acelerar(self):
         print("O carro acelerou!")
          
 class Freio:
@@ -11,8 +13,23 @@ class Carro:
         self.acelerador = Acelerador()
         self.freio = Freio()
         
-        
+    
+            
 carro = Carro()
 
 carro.acelerador.acelerar()
 carro.freio.freiar()
+
+# AGREGAÇÃO
+
+class Roda:
+    def __init__(self, tamanho):
+        self.tamanho = tamanhoç
+
+class Carro:
+    def __init__(self, modelo):
+        self.modelo = modelo
+        self.roda_dianteira_esquerda = Roda(15)
+        self.roda_dianteira_direita = Roda(15)
+        self.roda_traseira_esquerda = Roda(15)
+        self.roda_traseira_direita = Roda(15)
